12f92cffa8d17925882a40804d94a6afecbe31ff
YipWWong <71024678+YipWWong@users.noreply.github.com>
2020-09-12 18:35:20 +0100

    hyperlinks

--- src/main.js
+++ src/main.js
@@ -8,24 +8,35 @@ display the relevant text - this as a base will allow me to communicate with eac
  */
 
 var http = require('http'); //use the http module like create server
-var dt = require('./YipModule'); // load my module
 var url = require('url'); // access the url
 var fs = require('fs'); // file system
+var homepageFileName="src/homepage.html"; 
 
 http.createServer(function (req, res) {
     var q = url.parse(req.url, true);
     var filename = "src"+ q.pathname;
-    console.log(q.host); 
-    console.log(q.pathname); 
-    console.log(q.search);
-    console.log(filename); 
-    fs.readFile(filename, function(err, data) {
-        if (err) {
-            res.writeHead(404, {'Content-Type': 'text/html'});
-            return res.end("404 Not Found");
-          } 
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
-      });
+    console.log("host:"+q.host); 
+    console.log("pathname:"+q.pathname); 
+    console.log("search params:"+q.search);
+    console.log("filename:"+filename); 
+    
+    if (q.pathname == "/") {
+        readAndOutputFile(homepageFileName, res);
+      } else {
+        readAndOutputFile(filename, res);
+      }
+    
   }).listen(process.env.PORT);
+
+  
+  function readAndOutputFile(pathToFile, res) {
+    fs.readFile(pathToFile, function(err, data) {
+      if (err) {
+          res.writeHead(404, {'Content-Type': 'text/html'});
+          return res.end("404 Not Found");
+        } 
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      return res.end();
+    }); 
+  }
